74cd6fc2d90778a5e3d37761bdedf6dc439c5264
Your Actual Name <Your Actual Email>
2018-02-14 01:54:02 -0800

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,15 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map!{ |i| Integer(i)}
+    sum = 0
+    last_num = nil 
+    a.each do |i|
+    	if i % 2 == 0 and i < 10 and i != last_num
+    		sum = sum + i + 2
+    	end
+    	last_num = i 
+    end
+    return sum
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,19 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  squared_sum = (a + b) ** 2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort!
+  a.map! {|i| (i + 1)}
+  return a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -48,5 +51,5 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
+  word.split("").sum {|i| values[i.to_sym]}
 end
